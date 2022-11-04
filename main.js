8a9c40012f95c6bca75431131ee52943c0c4ae08
Olisa-tech <Okoloaustin16@gmail.com>
2022-11-04 17:52:19 +0800

    first project completed

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+// es 5
+
+// console.log("it is for garri")
+
+// var writeup = "who dey zazu"
+
+
+
+var harmburgerdiv = document.querySelector(".hamburger")
+var mobileLinks = document.querySelector(".mobile-links-holder")
+// console.log(harmburgerdiv)
+
+function agbacoder() {
+    harmburgerdiv.classList.toggle("showburger")
+    mobileLinks.classList.toggle("show-mobile-links-holder")
+}
